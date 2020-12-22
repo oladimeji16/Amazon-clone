ac8c42cf2e7c720bf9a8c222634fcb16d7f1838c
Oladimeji Mustapha <49591168+oladimeji16@users.noreply.github.com>
2020-12-22 13:53:31 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css';
+
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <Switch>
+          
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
